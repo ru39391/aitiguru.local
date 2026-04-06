c212dd716c7935301db9e4ef541edd7684e39516
ru39391 <dev@39391.ru>
2026-04-06 17:27:33 +0300

    feature/настроить проксирование

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+
+  return {
   plugins: [react()],
   build: {
     rollupOptions: {
@@ -20,6 +23,19 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 5173,
+    proxy: {
+      '/api': {
+        target: env.VITE_SITE_URL || "/",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      },
+      '/assets': {
+        target: env.VITE_SITE_URL || "/",
+        changeOrigin: true,
+        secure: false
+      }
+    }
   },
   resolve: {
     alias: {
@@ -27,4 +43,5 @@ export default defineConfig({
       "~": resolve(__dirname, "./assets"),
     },
   },
+}
 });
